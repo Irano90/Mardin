a1435a6b4dd1142859e1c6401536ead8e116236d
Irano90 <soc.brilliant@gmail.com>
2025-11-21 22:30:11 +0700

    Update author information in a_star.py

--- a_star.py
+++ a_star.py
@@ -1,7 +1,7 @@
 
-# Nama : Nama Kamu
-# NIM  : NIM Kamu
-# Mata Kuliah : Pencarian / AI (contoh, sesuaikan)
+# Nama : Brilliant Irano Mardin
+# NIM  : 32602200140
+# Mata Kuliah : Kecerdasan Buatan_FKCBD_20251
 
 import heapq
 import math
